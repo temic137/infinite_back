ec609f61ece3cf2daf5c4858c95c187c32d3f731
temic137 <68796483+temic137@users.noreply.github.com>
2024-09-20 17:21:40 +0200

    fixing the assets:precompile error in the deploy logs render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+
+  config.assets.compile = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
 
